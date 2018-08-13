04cbb2210376f1620cbb4f5caf5664ebb81059eb
azhilenko <azhilenko@gmail.com>
2018-08-13 09:19:46 +0300

    add scanner in main

--- src/main.java
+++ src/main.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class main {
     public static void main(String[] args) {
-        System.out.println("HEllo");
+        float val;
+        Scanner num = new Scanner(System.in);
+        System.out.print("Введите число с запятой: ");
+        val = num.nextFloat ();
+        System.out.println("Ваше число - " + val);
+
+
     }
 }
