a79ac2e9854a1bbeec6f49b37d4804b9f4a11beb
azhilenko <azhilenko@gmail.com>
2018-08-14 00:27:26 +0300

    array lenght

--- src/main.java
+++ src/main.java
@@ -2,11 +2,9 @@ import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) {
-        float val;
-        Scanner num = new Scanner(System.in);
-        System.out.print("Введите число с запятой: ");
-        val = num.nextFloat ();
-        System.out.println("Ваше число - " + val);
+        int arr[]={1,2,3,4};
+        System.out.println(arr.length-2);
+
 
 
     }
