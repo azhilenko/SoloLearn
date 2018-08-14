13ef5e49b37ea09c14b753aa133c65751a2b1d2e
azhilenko <azhilenko@gmail.com>
2018-08-15 01:21:22 +0300

    added getter and setter, get - out information, set - init.

--- src/MyClass.java
+++ src/MyClass.java
@@ -1,6 +1,11 @@
 public class MyClass {
     public static void main(String[] args) {
-        Animal dog = new Animal();
-        dog.bark();
+//        Animal dog = new Animal();
+//        dog.bark();
+        Vehicle v1=new Vehicle();
+        v1.setColor("Red");
+
+        System.out.println(v1.getColor());
+
     }
 }
--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,7 +1,18 @@
 public class Vehicle {
     int maxspeed, wheels;
-    String color;
+    private String color;
     double fuelcapacity;
+
+    public String getColor()
+    {
+        return color;
+    }
+    public  void setColor(String c)
+    {
+        this.color=c;
+    }
+
+
     void horn(){
         System.out.println("Beep!");
     }
