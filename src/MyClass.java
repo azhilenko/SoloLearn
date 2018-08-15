e8fc0b51c103aee77016e0a82ac8aa79cb1a6e06
azhilenko <azhilenko@gmail.com>
2018-08-16 02:54:02 +0300

    numeric and reference type (числовой и ссылочный тип), в первом случае мы оперируем над значением переменной, а не над ней самой.во втором метод действует на сам объект

--- src/MyClass.java
+++ src/MyClass.java
@@ -2,9 +2,23 @@ public class MyClass {
     public static void main(String[] args) {
 //        Animal dog = new Animal();
 //        dog.bark();
-        Vehicle v=new Vehicle("Blue");
+        int x=5;                    //numeric числовой
+        addOneTo(x);                //type тип
+        System.out.println(x);
 
-        System.out.println(v.getColor());
 
-    }
+        Person j;                   //reference ссылочный
+        j=new Person("John");   //type тип
+        j.setAge(20);
+        celebrateBirthday(j);
+        System.out.println(j.getAge());
+        }
+
+        static void celebrateBirthday(Person p){
+        p.setAge(p.getAge()+1);
+        }
+
+        static  void addOneTo(int num){
+        num=num=1; //при передаче в метод мы оперируем над значением, а не над самой переменной
+        }
 }
